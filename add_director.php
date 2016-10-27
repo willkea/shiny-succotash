97bfc973aff9908d30628b690c9016133860561d
Will Li <williamxli123@yahoo.com>
2016-10-26 23:09:00 -0700

    Add genre boxes

--- add_director.php
+++ add_director.php
@@ -10,24 +10,34 @@
 <form>
   <div class="form-group">
     <label for="d_f_name">First Name:</label>
-    <input type="text" class="form-control" id="d_f_name" placeholder="Enter First Name...">
+    <input type="text" class="form-control" name="d_f_name" placeholder="Enter First Name...">
   </div>
     
   <div class="form-group">
     <label for="d_l_name">Last Name:</label>
-    <input type="text" class="form-control" id="d_l_name" placeholder="Enter Last Name...">
+    <input type="text" class="form-control" name="d_l_name" placeholder="Enter Last Name...">
   </div>
     
     <div class="form-group">
     <label for="dob">Date of Birth: i.e. 1960-09-09</label>
-    <input type="text" class="form-control" id="dob" placeholder="1960-09-09">
+    <input type="text" class="form-control" name="dob" placeholder="1960-09-09">
   </div>
     <div class="form-group">
     <label for="dod">Date of Death: i.e. 1960-09-09, leave blank if alive</label>
-    <input type="text" class="form-control" id="dod" placeholder="1960-09-09">
+    <input type="text" class="form-control" name="dod" placeholder="1960-09-09">
   </div>
     
   <button type="submit" class="btn btn-default">Submit</button>
 </form>
+    
+<?php
+    $newFirstName = $_POST['d_f_name'];
+    $newLastName = $_POST['d_l_name'];
+    $newDOB = $_POST['dob'];
+    $newDOD = $_POST['dod'];
+    $getNewID_query = "SELECT * FROM MaxPersonID"
+    
+?>
+    
     
 </div>
--- add_movie.php
+++ add_movie.php
@@ -7,7 +7,7 @@
                 </div>
             </div>  
     
-<form>
+<form method="post">
   <div class="form-group">
     <label for="m_title">Title:</label>
     <input type="text" class="form-control" name="m_title" placeholder="Enter Movie Title...">
@@ -31,10 +31,73 @@
             <option>PG-13</option>
             <option>R</option>
             <option>NC-17</option>
-        </select></div>
+        </select>
+    </div>
     
-    
+
+<label>Genre(s): </label>
+    <div class="checkbox">
+      <label><input type="checkbox" name="formDoor[]" value="Action">Action&emsp;</label>     
+      <label><input type="checkbox" name="formDoor[]" value="Comedy">Comedy&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Adventure">Adventure&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Drama">Drama&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Adult">Adult&emsp;</label>
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="formDoor[]" value="Crime">Crime&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Western">Western&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Animation">Animation&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Family">Family&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Fantasy">Fantasy&emsp;</label>
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="formDoor[]" value="Horror">Horror&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Musical">Musical&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Mystery">Mystery&emsp;&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Thriller">Thriller&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="Sci-Fi">Sci-Fi&emsp;</label>
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="formDoor[]" value="Short">Short&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Romance">Romance&emsp;</label> 
+      <label><input type="checkbox" name="formDoor[]" value="Documentary">Documentary&emsp;</label>
+      <label><input type="checkbox" name="formDoor[]" value="War">War&emsp;</label>
+    </div>
+<br>
   <button type="submit" class="btn btn-default">Submit</button>
+    
 </form>
     
+<?php
+    $db = new mysqli('localhost', 'cs143', '', 'CS143'); 
+    
+    $newTitle = $_POST["m_title"];
+    $newCompany = $_POST["m_company"];
+    $newYear = $_POST["m_year"];
+    $newRating = $_POST["m_rating"];
+    $aDoor = $_POST['formDoor'];
+    $N = count($aDoor);
+    echo("You selected $N door(s): ");
+     for($i=0; $i < $N; $i++)
+    {
+      echo($aDoor[$i] . " ");}
+    //$mid_query = "SELECT id FROM Movie WHERE title='". $newTitle. "'";
+   
+    //$mid = mysqli_query($db, $mid_query);
+
+    //$row = mysqli_fetch_row($mid);
+    //$row2 = mysqli_fetch_row($did);
+    //$insert_query = "INSERT INTO MovieDirector(mid,did) VALUES('".$row[0]."', '".$row2[0]."')";
+    //if ( $db->query($insert_query) === TRUE ){
+        //echo "<br>"."New record created successfully";
+    //} 
+    $db->close();
+?>
+    
 </div>
+
+
+
+
+
+
