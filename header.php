85f7c69bcd80dce6763ea98d1ee34bc7b55d1eb6
Will Li <williamxli123@yahoo.com>
2016-10-27 14:28:14 -0700

    Start search page

--- header.php
+++ header.php
@@ -265,14 +265,16 @@
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
                         <li class="sidebar-search">
+                            <form method="post" action="search.php">
                             <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
+                                <input type="text" name="searchInput" class="form-control" placeholder="Search...">
                                 <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
+                                <button class="btn btn-default" type="submit">
                                     <i class="fa fa-search"></i>
                                 </button>
-                            </span>
+                                </span>
                             </div>
+                            </form>
                             <!-- /input-group -->
                         </li>
                           <li>
---
+++ search.php
@@ -0,0 +1,31 @@
+<?php require_once('header.php'); ?>
+
+<div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Search Results</h1>
+                </div>
+            </div>  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
