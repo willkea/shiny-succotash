f07647f2b0e5916fd67929f25abcc83ab2d890c2
Will Li <williamxli123@yahoo.com>
2016-10-28 12:06:52 -0700

    Add show actors

--- show_actors.php
+++ show_actors.php
@@ -6,11 +6,111 @@
                     <h1 class="page-header">Browse Actors</h1>
                 </div>
             </div>  
+ 
+<?php
+    $db = new mysqli('localhost', 'cs143', '', 'CS143');
+    $user_query = "SELECT concat(first,' ',last) FROM Actor;";
+    $result = $db->query($user_query);
+?>    
     
+<form method="get">
+    <div class="form-group">
+    <label>Actor: </label>
+    <select name='actor' class="form-control">";
+        <?php 
+        echo "<option disabled selected>Select an actor...</option>";
+        while($row = mysqli_fetch_row($result))
+        {
+        echo "<option>";
+        // $row is array... foreach( .. ) puts every element
+        // of $row to $cell variable
+        foreach($row as $cell)
+            echo "$cell";
+        echo "</option>";
+        } ?>
+    </select>
+    </div> 
+     <button type="submit" name="submit" class="btn btn-default">Submit</button>
+</form><br>
     
-<?php    
-  $searchInput = $_POST['searchInput']; 
+<?php
+     if(isset($_GET['submit'])){
+         $newSearch = $_GET["actor"];
+         $aid_query = "SELECT CONCAT(first,' ',last) Name, sex, dob AS DOB, dod AS DOD FROM Actor WHERE CONCAT(first,' ',last)='$newSearch'";
+         
+         $roles_query = "SELECT mid, role FROM MovieActor,Actor WHERE CONCAT(first,' ',last)='$newSearch' AND id=aid";
+         
+         $result = mysqli_query($db, $aid_query);
+         $result2 = mysqli_query($db, $roles_query);
+         $field_nums = mysqli_num_fields($result);
+         $field_nums2 = mysqli_num_fields($result2);
+         
+              //movie table   
+    echo "<div class='panel panel-default'><div class='panel-heading'>Actor Information:</div><div class='panel-body'><div class='table-responsive'><table class='table table-striped table-bordered table-hover'><thead><tr>";
+    // printing table headers
+    for($i=0; $i<$field_nums; $i++)
+    {
+        $field = $result->fetch_field();
+        $fieldtitle = ucfirst($field->name);
+        echo "<th>$fieldtitle</th>";
+    }
+    echo "</tr></thead><tbody>";
+    // printing table rows
+    while($row = mysqli_fetch_row($result))
+    {
+        echo "<tr>";
+        // $row is array... foreach( .. ) puts every element
+        // of $row to $cell variable
+        foreach($row as $cell)
+            echo "<td>$cell</td>";
+        echo "</tr>\n";
+    }
+        echo "</tbody></table></div></div></div>";
+    $result->free();
+         
+     //roles table     
+   echo "<div class='panel panel-default'><div class='panel-heading'>Actor Movies and Roles:</div><div class='panel-body'><div class='table-responsive'><table class='table table-striped table-bordered table-hover'><thead><tr>";
+    // printing table headers
+    for($i=0; $i<$field_nums2; $i++)
+    {
+        $field2 = $result2->fetch_field();
+        $fieldtitle2 = ucfirst($field2->name);
+        echo "<th>$fieldtitle2</th>";
+    }
+    echo "</tr></thead><tbody>";
+    // printing table rows
+    while($row = mysqli_fetch_row($result2))
+    {
+        echo "<tr>";
+        // $row is array... foreach( .. ) puts every element
+        // of $row to $cell variable
+        foreach($row as $cell)
+            echo "<td>$cell</td>";
+        echo "</tr>\n";
+    }
+        echo "</tbody></table></div></div></div>";
+    $result2->free();         
+         
+     }
+    
 ?>
-     
+
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
